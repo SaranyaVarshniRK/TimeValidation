595f6990f077dd9951a4e0595c73dada980ba119
SaranyaVarshniRK <saranyamailofficial1996@gmail.com>
2018-10-29 17:15:23 +0530

    testing the fourth changes

--- Sample/src/Split.java
+++ Sample/src/Split.java
@@ -5,9 +5,10 @@ public class Split {
 	
 	public static void main(String args[])
 	{
+		String newName="ABCD";
 		String number="5465982136";
 		String p1=number.substring(6,10);
-		
+		//New change
 		
 		System.out.println(p1);
 		//System.out.println("New change");
